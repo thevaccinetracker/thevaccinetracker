a0f67127a1e4a970725752dd32441d694d0751f8
ujjwal8880 <ujjwal.singhal@rentomojo.com>
2020-05-30 08:54:39 +0530

    converted index.js to component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
-import React from "react"
+import React, {Component} from "react"
 import "../assets/styles/style.scss"
 
-const IndexPage = () => (
-  <div className="container-fluid">
-    <h1>
-      Welcome <span className="glyphicon glyphicon-heart"></span>
-    </h1>
-  </div>
-)
+class IndexPage extends Component {
+
+  state = {
+    name : 'ujjwal',
+  }
+
+  render() {
+    const {name} = this.state;
+    return (
+      <div className="container-fluid">
+        <h1>
+          Hello From {name} <span className="glyphicon glyphicon-heart"></span>
+        </h1>
+      </div>
+    );
+  }
+}
 
 export default IndexPage
