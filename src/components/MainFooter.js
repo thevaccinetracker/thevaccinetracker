578f1747543d80cb13b9bd6f43637c1a276a8028
Rohit Bind <rohit.bind@gmail.com>
2020-06-13 19:25:43 +0530

    added rel

--- src/components/MainFooter.js
+++ src/components/MainFooter.js
@@ -13,9 +13,9 @@ class MainHeader extends Component {
           </div>
           <div className="col-md-4 text-right">
             <ul>
-              <li><a href="https://rohitbind.typeform.com/to/Kso14D" target="_blank">Collaborate with us</a></li>
-              <li><a href="https://github.com/thevaccinetracker/thevaccinetracker" target="_blank">Github</a></li>
-              <li><a href="https://www.figma.com/file/8rXo6Cu59WpdOkXdQdi1Ce/The-Vaccine-Tracker-(-thevaccinetracker.com-)?node-id=0%3A1" target="_blank">Figma</a></li>
+              <li><a href="https://rohitbind.typeform.com/to/Kso14D" target="_blank" rel="noreferrer">Collaborate with us</a></li>
+              <li><a href="https://github.com/thevaccinetracker/thevaccinetracker" target="_blank" rel="noreferrer">Github</a></li>
+              <li><a href="https://www.figma.com/file/8rXo6Cu59WpdOkXdQdi1Ce/The-Vaccine-Tracker-(-thevaccinetracker.com-)?node-id=0%3A1" target="_blank" rel="noreferrer">Figma</a></li>
             </ul>
           </div>
         </div>
