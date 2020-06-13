ca3033b144834a4c3eb1e7127459ede477e51572
Rohit Bind <rohit.bind@gmail.com>
2020-06-13 21:24:16 +0530

    adding resources & data link

--- src/components/MainFooter.js
+++ src/components/MainFooter.js
@@ -6,17 +6,18 @@ class MainHeader extends Component {
     return (
       <div className="container mainFooter">
         <div className="row">
-          <div className="col-md-8">
+          <div className="col-md-7">
             <p className="paragraph">
               It took us 4 weekend to bulid this. But for you it will take only 5 sec to share. We have interesting features in the pipeline. Please do SHARE.
             </p>
           </div>
-          <div className="col-md-4 text-right">
+          <div className="col-md-5 text-right">
             <ul>
               <li><a href="https://rohitbind.typeform.com/to/Kso14D" target="_blank" rel="noreferrer">Collaborate with us</a></li>
               <li><a href="https://github.com/thevaccinetracker/thevaccinetracker" target="_blank" rel="noreferrer">Github</a></li>
               <li><a href="https://www.figma.com/file/8rXo6Cu59WpdOkXdQdi1Ce/The-Vaccine-Tracker-(-thevaccinetracker.com-)?node-id=0%3A1" target="_blank" rel="noreferrer">Figma</a></li>
               <li><a href="https://forms.gle/6DDNX4b74NMRw6GH8" target="_blank" rel="noreferrer">Feedback</a></li>
+              <li><a href="https://docs.google.com/spreadsheets/d/1ImpYv9-_qKmF8JkdV8YW1tN8IAycPszPpG6VCn-rH4Q/edit?usp=sharing" target="_blank" rel="noreferrer">Resources &amp; Data</a></li>
             </ul>
           </div>
         </div>
