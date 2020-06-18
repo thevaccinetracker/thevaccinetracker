f3a4bc4d3e8a6cc168e500cba504938eda3b642b
ujjwal8880 <ujjwal.singhal@rentomojo.com>
2020-06-18 15:24:29 +0530

    fixed route warning and added background image

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.onCreatePage = async ({page, actions}) => {
   const {createPage} = actions;
   if (page.path.match(/^\/developer/)){
     createPage({
-      path : '/developer',
+      path : '/developer/',
       matchPath : '/developer/*',
       component : path.resolve('src/pages/developer.js')
     })
