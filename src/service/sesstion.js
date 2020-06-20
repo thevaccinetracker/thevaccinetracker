26616e481538960ce86540e80c3be9db356b462d
ujjwal8880 <ujjwal.singhal@rentomojo.com>
2020-06-20 23:25:53 +0530

    fixed some warning issue

--- src/service/sesstion.js
+++ src/service/sesstion.js
@@ -1,12 +1,12 @@
-const { vaccineObj } = require("../contants/conts");
+// const { vaccineObj } = require("../contants/conts");
 
 const Session = {
-    get vaccineList() {
-        return JSON.parse(sessionStorage.getItem("vaccineList"));
-    },
-    set vaccineList(value) {
-        sessionStorage.setItem("vaccineList", JSON.stringify(value));
-    }
+  get vaccineList() {
+    return JSON.parse(sessionStorage.getItem('vaccineList'))
+  },
+  set vaccineList(value) {
+    sessionStorage.setItem('vaccineList', JSON.stringify(value))
+  }
 }
 
 export { Session }
